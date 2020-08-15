6071da3b483ad601ebf950acf4b58d56444a479f
Luis Blecha <lh.blecha@hotmail.com>
2020-08-15 16:33:45 -0400

    Criando o component NavBar

---
+++ src/components/Home.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <v-card>
+            <v-card-title>Título do Card</v-card-title>
+            <v-card-text>Texto do Card </v-card-text>
+        </v-card>
+    </div>
+</template>
